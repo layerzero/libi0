080b245801de93d860d497d713c1db5832a832ae
progman <progman@ubuntu.(none)>
2014-06-17 16:46:43 +0800

    continue work

--- libi0/stddef.h
+++ libi0/stddef.h
@@ -198,28 +198,10 @@ register long reg4;
 #define NEW_RUNNER_ID_ADDR (0x100000410)
 #define EC_RANGE           (0x1000)
 
-// functions
+long get_my_task_id();
 
-// get current task's ID
-long get_my_task_id()
-{
-    return *(long*)(RUNNER_ID);
-}
+long get_new_task_id();
 
-// get the new tasks' ID.
-// should be called just after invoking "runner" or spawn/spawnx
-long get_new_task_id()
-{
-    return *(long*)(NEW_RUNNER_ID_ADDR);
-}
-
-// calculate the exit code range from the ID
-// len in bytes
-void task_id_to_ec_range(long id, long *base, long *len)
-{
-    *base = id;
-    *len = EC_RANGE;
-    return;
-}
+void task_id_to_ec_range(long id, long *base, long *len);
 
 #endif // I0STDDEF_H
