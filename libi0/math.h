cfbc83b490947099cdaa3f1f00db7a0ac6dd2d49
Eric Zhiqiang Ma <zma@ericzma.com>
2014-06-21 20:29:14 +0800

    porting an exp implementation

--- libi0/math.h
+++ libi0/math.h
@@ -5,8 +5,7 @@ long ipow(long base, long exp)
 {
     long result;
     result = 1;
-    while (exp)
-    {
+    while (exp) {
         if ((exp & 1) == 1)
             result = result * base;
         exp = exp / 2;
