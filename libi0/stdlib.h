074218c972fbdacae4e5a13a9ada373478abbfd0
Eric Zhiqiang Ma <zma@ericzma.com>
2014-09-17 23:50:37 +0800

    support systemEx and system

--- libi0/stdlib.h
+++ libi0/stdlib.h
@@ -3,6 +3,10 @@
 
 #include "stddef.h"
 
+#define option_block_t (char*)
+
 long str_to_long(char* str, long maxlen, long *pnext, long *result);
+long systemEx(char *command, option_block_t option);
+long system(char *command);
 
 #endif // I0STDLIB_H
