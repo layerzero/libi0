a9bf96ebfe58de87e65d1a614af048a86f12198f
Eric Zhiqiang Ma <zma@ericzma.com>
2014-11-04 16:17:34 +0800

    implement libi0 interfaces

--- libi0/bigmsg.h
+++ libi0/bigmsg.h
@@ -1,4 +1,14 @@
 #ifndef _BIGMSG_H_
 #define _BIGMSG_H_
 
+#include "stdlib.h"
+
+#define msg_option_t char*
+
+option_block_t msg_option_to_option(msg_option_t option);
+
+long systemMsg(char *command, msg_option_t option);
+
+msg_option_t create_single_msg_option(char *addr, uint64_t len);
+
 #endif // _BIGMSG_H_
